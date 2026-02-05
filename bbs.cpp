e933548fd22007e481035aad2c0b1a4881b27311
ranker <tigrlev2007@gmail.com>
2026-02-05 13:26:55 +0300

    Add files via upload

---
+++ bbs.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+
+p = (char**)malloc(6 * sizeof(char*));
+for(int i = 0; i < 6; i++) {
+    p[i] = b[i];
+}
---
+++ contest1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+unsigned long long  ter;
+    scanf("%llx", &ter);
+    printf("%lld",ter);
+}
