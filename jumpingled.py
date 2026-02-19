7e5ef0e1f66e6295ef12a4cb82f1816a6ecb9d11
ranker <tigrlev2007@gmail.com>
2026-02-19 13:03:18 +0300

    Add files via upload

--- jumpingled.py
+++ jumpingled.py
@@ -1,63 +1,42 @@
 import RPi.GPIO as GPIO
+import time
 
-class R2R_DAC:
-    def init(self, gpio_bits, dynamic_range, verbose=False):
-        self.gpio_bits = gpio_bits
-        self.dynamic_range = dynamic_range
-        self.verbose = verbose
+GPIO.setmode(GPIO.BCM)
 
-        GPIO.setmode(GPIO.BCM)
-        for pin in self.gpio_bits:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, GPIO.LOW)
+LED_PINS = [16, 20, 21, 25, 26, 17, 27, 22]
+DYNAMIC_RANGE = 3.183
 
-        if self.verbose:
-            print(f"Инициализирован ЦАП с пинами {gpio_bits}, диапазон {dynamic_range}В")
+for pin in LED_PINS:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
 
-    def deinit(self):
-        GPIO.output(self.gpio_bits, GPIO.LOW)
-        GPIO.cleanup()
-        if self.verbose:
-            print("GPIO очищены")
+def voltage_to_binary(voltage):
+    if voltage < 0 or voltage > DYNAMIC_RANGE:
+        print(f"Напряжение выходит за диапазон (0 - {DYNAMIC_RANGE} В)")
+        return None
 
-    def set_number(self, number):
-        if number < 0 or number > 255:
-            if self.verbose:
-                print(f"Число {number} выходит за пределы 0-255")
-            return
+    number = int((voltage / DYNAMIC_RANGE) * 255)
+    bits = [(number >> i) & 1 for i in range(7, -1, -1)]
 
-        bits = [(number >> i) & 1 for i in range(7, -1, -1)]
-        GPIO.output(self.gpio_bits, bits)
+    print(f"Напряжение: {voltage} В")
+    print(f"Число: {number}")
+    print(f"Биты: {bits}")
 
-        if self.verbose:
-            print(f"Число на вход ЦАП: {number}, биты: {bits}")
+    return bits
 
-    def set_voltage(self, voltage):
-        max_voltage = self.dynamic_range
-        min_voltage = 0.0
+def set_leds(bits):
+    if bits:
+        GPIO.output(LED_PINS, bits)
 
-        if voltage < min_voltage or voltage > max_voltage:
-            print(f"Напряжение выходит за динамический диапазон ЦАП ({min_voltage:.2f} - {max_voltage:.2f} В)")
-            return
+try:
+    while True:
+        try:
+            voltage = float(input("Введите напряжение в Вольтах: "))
+            bits = voltage_to_binary(voltage)
+            set_leds(bits)
+            time.sleep(0.5)
+        except ValueError:
+            print("Ошибка: введите число")
 
-        number = int((voltage / max_voltage) * 255)
-        self.set_number(number)
-
-if name == "main":
-    dac = None
-    try:
-        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
-
-        while True:
-            try:
-                voltage = float(input("Введите напряжение в Вольтах: "))
-                dac.set_voltage(voltage)
-            except ValueError:
-                print("Вы ввели не число. Попробуйте еще раз")
-
-    except KeyboardInterrupt:
-        print("\nПрограмма прервана пользователем")
-
-    finally:
-        if dac:
-            dac.deinit()
+except KeyboardInterrupt:
+    GPIO.cleanup()
