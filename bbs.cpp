e70c40af28549455db946333ee4f6675e8830838
ranker <tigrlev2007@gmail.com>
2026-02-26 11:36:01 +0300

    Update bbs.cpp

--- bbs.cpp
+++ bbs.cpp
@@ -1,50 +1,39 @@
 import RPi.GPIO as GPIO
 
 class R2R_DAC:
-    def init(self, gpio_bits, dynamic_range, verbose=False):
+    def init(self, gpio_bits, dynamic_range):
         self.gpio_bits = gpio_bits
         self.dynamic_range = dynamic_range
-        self.verbose = verbose
         
         GPIO.setmode(GPIO.BCM)
         for pin in self.gpio_bits:
             GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, GPIO.LOW)
-        
-        if self.verbose:
-            print(f"ЦАП инициализирован. Пины: {gpio_bits}")
-            print(f"Динамический диапазон: {dynamic_range} В")
     
     def deinit(self):
-        GPIO.output(self.gpio_bits, GPIO.LOW)
+        GPIO.output(self.gpio_bits, 0)
         GPIO.cleanup()
-        if self.verbose:
-            print("GPIO очищены")
     
-    def set_number(self, number):
-        if number < 0 or number > 255:
-            if self.verbose:
-                print(f"Ошибка: число {number} вне диапазона 0-255")
-            return
-        
+    def voltage_to_number(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавливаем 0.0 В")
+            return 0
+        return int(voltage / self.dynamic_range * 255)
+    
+    def number_to_dac(self, number):
         bits = [(number >> i) & 1 for i in range(7, -1, -1)]
         GPIO.output(self.gpio_bits, bits)
-        
-        if self.verbose:
-            print(f"Число: {number}, биты: {bits}")
+        print(f"Число на вход ЦАП: {number}, биты: {bits}")
     
     def set_voltage(self, voltage):
-        if voltage < 0 or voltage > self.dynamic_range:
-            print(f"Напряжение {voltage} В вне диапазона (0 - {self.dynamic_range:.2f} В)")
-            return
-        
-        number = int((voltage / self.dynamic_range) * 255)
-        self.set_number(number)
+        number = self.voltage_to_number(voltage)
+        self.number_to_dac(number)
 
 if name == "main":
     dac = None
     try:
-        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.3)
         
         while True:
             try:
