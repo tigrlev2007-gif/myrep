3c022ea30c5c1cfa5ccc1a5fce2b55917684e729
ranker <tigrlev2007@gmail.com>
2026-02-26 13:42:54 +0300

    Update bbs.cpp

--- bbs.cpp
+++ bbs.cpp
@@ -1,18 +1,39 @@
-def get_triangle_voltage(freq=1, amp=1.65, offset=1.65):
-    """Возвращает текущее напряжение треугольной формы"""
-    t = time.time()
-    period = 1.0 / freq
-    t_mod = (t % period) / period
-    
-    if t_mod < 0.5:
-        val = 4 * t_mod - 1
-    else:
-        val = 3 - 4 * t_mod
-    
-    return offset + amp * val
+import r2r_dac as r2r
+import signal_generator as sg
+import time
 
-# В цикле программы:
-while True:
-    voltage = get_triangle_voltage(freq=10, amp=1.5, offset=1.65)
-    dac.set_voltage(voltage)
-    time.sleep(0.001)stop()
+# Параметры сигнала
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+
+try:
+    # Создаём объект R2R ЦАП
+    dac = r2r.R2RDAC()
+
+    print(f"Генерация синусоиды: {signal_frequency}Гц, амплитуда {amplitude}В, "
+          f"дискретизация {sampling_frequency}Гц")
+
+    while True:
+        # Текущее время
+        current_time = time.time()
+
+        # Амплитуда синусоиды (0..1)
+        sin_amplitude = sg.get_sin_wave_amplitude(signal_frequency, current_time)
+
+        # Напряжение = амплитуда * коэффициент
+        voltage = amplitude * sin_amplitude
+
+        # Выдаём на ЦАП
+        dac.set_voltage(voltage)
+
+        # Ждём период дискретизации
+        sg.wait_for_sampling_period(sampling_frequency)
+
+except KeyboardInterrupt:
+    print("\nОстановка генератора по Ctrl+C")
+
+finally:
+    # Закрываем ЦАП
+    dac.deinit()
+    print("ЦАП отключён")
