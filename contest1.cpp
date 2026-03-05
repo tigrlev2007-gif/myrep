2cc6a7fefac58617bdae35faf947453b8345c08f
ranker <tigrlev2007@gmail.com>
2026-03-05 11:53:25 +0300

    Update contest1.cpp

--- contest1.cpp
+++ contest1.cpp
@@ -1,18 +1,23 @@
-import numpy as np
+import pwm_dac
+import signal_generator as sg
 import time
 
-def get_sin_wave_amplitude(freq, time_moment):
-    sin_val = np.sin(2 * np.pi * freq * time_moment)
-    return (sin_val + 1) / 2
+amplitude = 3.290
+signal_frequency = 10
+sampling_frequency = 1000
 
-def get_triangle_wave_amplitude(freq, time_moment):
-    period = 1 / freq
-    relative_time = time_moment % period
-    if relative_time < period / 2:
-        return (2 / period) * relative_time
-    else:
-        return 2 - (2 / period) * relative_time
+try:
+    dac = pwm_dac.PWM_DAC(gpio_pin=12, pwm_frequency=500, dynamic_range=3.290, verbose=False)
+    start_time = time.time()
+    
+    while True:
+        current_time = time.time() - start_time
+        relative_amplitude = sg.get_sin_wave_amplitude(signal_frequency, current_time)
+        voltage = relative_amplitude * amplitude
+        dac.set_voltage(voltage)
+        sg.wait_for_sampling_period(sampling_frequency)
 
-def wait_for_sampling_period(sampling_frequency):
-    time.sleep(1 / sampling_frequency)
+finally:
+    dac.deinit()
+
 
